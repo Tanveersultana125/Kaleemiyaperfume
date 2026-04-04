b4173bcaf71e06821da1b4bb959d4f1a32ff4d04
Tanveersultana125 <tanveersultana84@gmail.com>
2026-04-04 18:25:15 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig(({ mode }) => ({
             src: "/official-brand-icon-512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "maskable",
+            purpose: "any",
           },
         ],
       },
